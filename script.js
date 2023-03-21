ee799015beeef20f079e169fca44184b4c2475e2
Zach5812 <zachlouk7@gmail.com>
2023-03-21 15:47:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var likes=0
+function increaseLikes(element){
+    var likesOne = document.querySelector("#likes1");
+    likes++;
+    likesOne.innerHTML=likes + " likes";
+}
+
+var liked2=0
+function increaseLikes2(element){
+    var likesTwo = document.querySelector("#likes2");
+    liked2++;
+    likesTwo.innerHTML=liked2 + " likes";
+}
+
+var liked3=0
+function increaseLikes3(element){
+    var likesThree = document.querySelector("#likes3");
+    liked3++;
+    likesThree.innerHTML=liked3 + " likes";
+}
